cd784d48877edf901e89265ef9c4041de2f1baee
Louissong79 <cas1205@naver.com>
2019-05-21 12:56:50 +0900

    Customer Database

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const fs = require('fs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const data = fs.readFileSync(`./database.json`);
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
 
 /*
 app.get('/api/hello', (req, res) => {
     res.send( {message: "Hello Express Louis"});
 });
-*/
+
 app.get('/api/customers', (req, res) => {
     res.send( [
         
@@ -40,6 +56,16 @@ app.get('/api/customers', (req, res) => {
             }                 
 
     ]);
+});
+*/
+
+app.get('/api/customers', (req, res) => {
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
